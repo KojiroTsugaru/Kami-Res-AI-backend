83668b702593a84df3a2c485d21a0a2276cf30be
KojiroTsugaru <kojirotsugaru@gmail.com>
2025-07-27 13:27:56 -0700

    enabled debug mode

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from app.api.routes import router as api_router
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 app.include_router(api_router, prefix="/api/v1")
 
